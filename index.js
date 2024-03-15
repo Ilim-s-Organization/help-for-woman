8bb4dfc8e7ea50bfb3d0899c9402d0495d713ee6
Ramazan <148753896+RaMMzan2024@users.noreply.github.com>
2024-03-15 14:57:35 +0600

    added find JS

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ let a = 5
 let b = 10
 
 console.log(a + b, 'Hello')
+
+function find_openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("find_tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("find_tab");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
+      tablinks[i].style.borderBottomColor = "transparent";
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+    evt.currentTarget.style.borderBottomColor = "#007bff";
+    moveLine(evt.currentTarget);
+  }
