f65c8365db0da40252a3552550a2d596e48959d1
Ilimchik <ilimbork@gmail.com>
2024-03-17 11:05:55 +0600

    ch swiper

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-let a = 5
-let b = 10
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-console.log(a + b, 'Hello')
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
 
 function find_openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("find_tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("find_tab");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove("active");
-      tablinks[i].style.borderBottomColor = "transparent";
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.classList.add("active");
-    evt.currentTarget.style.borderBottomColor = "#007bff";
-    moveLine(evt.currentTarget);
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("find_tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("find_tab");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+    tablinks[i].style.borderBottomColor = "transparent";
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+  evt.currentTarget.style.borderBottomColor = "#007bff";
+  moveLine(evt.currentTarget);
+}
